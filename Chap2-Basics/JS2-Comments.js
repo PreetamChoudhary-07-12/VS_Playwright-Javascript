1d69880652a2b3acf23cb58154e19b9edd4fe47d
PreetamChoudhary <pritamgurjar222@gmail.com>
2026-02-16 10:05:28 +0530

    Add Chapter 2 JavaScript files - Comments and var/keywords/literal/Identifier lessons

---
+++ Chap2-Basics/JS2-Comments.js
@@ -0,0 +1,13 @@
+
+
+
+// this is preetam
+console.log("Hello World"); // this is a comment
+
+
+/**
+ * this is multi line comment 
+ * author - preetam choudharu
+ * date - 14-feb-2026
+ * 
+ **/
---
+++ Chap2-Basics/JS3-var,keywords,literal,Identifier.js
@@ -0,0 +1,37 @@
+
+// var
+var message = "Hello";
+console.log("var example:", message);
+
+// let
+let count = 5;
+count += 1;
+console.log("let example:", count);
+
+// const
+const pi = 3.14;
+console.log("const example:", pi);
+
+// keyword (using 'if' keyword)
+let temperature = 30;
+if (temperature > 25) {
+  console.log("keyword example: It's warm!");
+}
+
+// identifier
+let userName = "Preetam";
+console.log("identifier example:", userName);
+
+// Literal
+let greeting = "Hi"; // string literal
+let age = 25; // numeric literal
+let isActive = true; // boolean literal
+console.log("literal example:", greeting, age, isActive);
+
+// Operators
+let a = 10;
+let b = 3;
+console.log("operators example:");
+console.log("Addition:", a + b);
+console.log("Comparison:", a > b);
+console.log("Logical:", a > 5 && b < 5);
