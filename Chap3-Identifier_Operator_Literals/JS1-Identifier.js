c94c701e5064fd7fb2a47eb7669e5c03ebc14fd0
PreetamChoudhary <pritamgurjar222@gmail.com>
2026-02-23 20:10:14 +0530

    Add JavaScript practice files - Chap1, Chap3, and Chap5 exercises

---
+++ Chap3-Identifier_Operator_Literals/JS1-Identifier.js
@@ -0,0 +1,19 @@
+/**Best Practice Tip
+ * Stick to camelCase for variables/methods 
+ * PascalCase for classes
+*/
+
+let myVariable = 42;
+console.log(myVariable);
+
+let variable = "hi";
+console.log(variable);
+
+//'let' is not allowed to be used as a name in 'let' or 'const' declarations.
+//const let = 'A';
+
+// 'const' is not allowed to be used as a name in 'let' or 'const' declarations.
+//let const = 10.20; 
+
+//Cannot redeclare block-scoped variable 'variable'
+//var variable = 123456789;
