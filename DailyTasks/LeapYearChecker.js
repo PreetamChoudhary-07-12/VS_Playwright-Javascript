bcdee19bda0e7bb9392f9d0312dd5052c3785c3f
PreetamChoudhary <pritamgurjar222@gmail.com>
2026-03-03 08:08:29 +0530

    Update: Install Playwright and add test directories with package dependencies

--- DailyTasks/LeapYearChecker.js
+++ DailyTasks/LeapYearChecker.js
@@ -6,7 +6,7 @@
 // year = 2026 , no
 
 let year=2028;
- if(year%4==0&&year%100!=0 || year%400==0){
+ if(year % 4==0 && year % 100!=0 || year % 400==0 ){
     console.log(`${year} is a leap year.`);
 }
 else{
---
+++ PW_JS_Test2/t.js
@@ -0,0 +1,22 @@
+// console.log(x); 
+//  x = 5;
+
+//  for (var i=0;i<5;i++){
+//     console.log(i);
+//  }
+ 
+ //let _privateVar= 10;
+// console.log(x); 
+
+// const arr = [1, 2, 3]; 
+// arr.push(4); 
+// console.log(arr); 
+//console.log(2 ** 3);
+// console.log('5' + 3);
+// console.log('5' - 3);
+// console.log(true + true);
+// console.log(10 % 3);
+// let a = 5; let b = a++; console.log(a, b);
+//if ('') { console.log('yes'); } else { console.log('no'); }
+console.log(Boolean([]));
+for (let i = 0; i < 5; i++) { if (i === 3) break; } console.log(i);
